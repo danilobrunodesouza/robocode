af728c51debc4fc57d3eac42fb4135c8c0d1b10e
Danilo Bruno de Souza <danilobrunodesouza@gmail.com>
2017-05-20 09:24:18 -0300

    Magneto Finalizado

--- src/time/Magneto.java
+++ src/time/Magneto.java
@@ -11,42 +11,52 @@ import robocode.HitWallEvent;
 import robocode.ScannedRobotEvent;
 import robocode.WinEvent;
 
+//ajustar a mira
 public class Magneto extends AdvancedRobot {
 
 	private double distanciaInimigo;
 	private double anguloDoInimigo;
+	private double energiaDoInimigo;
 
 	private int acertos = 0;
 	private int erros = 0;
 
 	@Override
 	public void run() {
-		setColors(Color.RED, Color.BLUE, Color.BLUE);
+		setColors(defineCores(108, 12, 1), defineCores(50, 34, 103), defineCores(50, 34, 103));
+		setScanColor(defineCores(108, 12, 1));
+		setBulletColor(defineCores(182, 250, 251));
 		while (true) {
 			jogaNormal();
 			this.execute();
-
 		}
 
 	}
 
+	public Color defineCores(int r, int g, int b) {
+		return new Color(r, g, b);
+	}
+
+	public void miraNoInimigo(ScannedRobotEvent inimigo) {
+		anguloDoInimigo = inimigo.getBearing();
+		setTurnGunRight((relatividadeAngular(anguloDoInimigo + getHeading() - getGunHeading())));
+	}
+
 	public void tiroCerto(ScannedRobotEvent inimigo) {
 		distanciaInimigo = inimigo.getDistance();
-		anguloDoInimigo = inimigo.getBearing();
-		
-		setTurnGunRight((relatividadeAngular(anguloDoInimigo + getHeading() - getGunHeading())));
-		if (inimigo.getEnergy() < 12) {
+		energiaDoInimigo = inimigo.getEnergy();
+
+		if (energiaDoInimigo < 12 && distanciaInimigo < 300) {
 			fire((inimigo.getEnergy() / 4) + .1);
-		} else if (distanciaInimigo <= 200) {
-			fire(3);
-		} else if (distanciaInimigo > 200 && distanciaInimigo <= 325) {
+		} else if (distanciaInimigo <= 150) {
+			fireBullet(3);
+		} else if (distanciaInimigo > 150 && distanciaInimigo <= 225) {
 			fire(2);
-		} else if (distanciaInimigo > 325 && distanciaInimigo < 500) {
+		} else if (distanciaInimigo > 225 && distanciaInimigo < 300) {
 			fire(1);
 		} else {
 			turnRight(relatividadeAngular(anguloDoInimigo));
-			ahead(distanciaInimigo * 0.3);
-			fire(1);
+			setAhead(distanciaInimigo * 0.3);
 		}
 
 	}
@@ -65,7 +75,8 @@ public class Magneto extends AdvancedRobot {
 
 	@Override
 	public void onScannedRobot(ScannedRobotEvent inimigoNoRadar) {
-		defineEstrategia(inimigoNoRadar);
+		// defineEstrategia(inimigoNoRadar);
+		miraNoInimigo(inimigoNoRadar);
 		tiroCerto(inimigoNoRadar);
 	}
 
@@ -91,50 +102,41 @@ public class Magneto extends AdvancedRobot {
 	public void onHitRobot(HitRobotEvent event) {
 		anguloDoInimigo = event.getBearing();
 		turnGunRight(relatividadeAngular(anguloDoInimigo + getHeading() - getGunHeading()));
+		setBack(100);
+		setTurnRight(90);
 		fireBullet(3);
 	}
 
 	public double relatividadeAngular(double angulo) {
-		if (angulo > -180 && angulo <= 180) {
+		if (angulo >= -180 && angulo <= 180) {
 			return angulo;
 		}
 		double miraRapida = angulo;
-		while (miraRapida <= -180) {
-			miraRapida += 360;
-		}
-		while (angulo > 180) {
-			miraRapida -= 360;
-		}
+		if (miraRapida >  180) miraRapida -= 180;
+		if (miraRapida < -180) miraRapida += 180;
 		return miraRapida;
+
 	}
 
-	public void defineEstrategia(ScannedRobotEvent inimigo) {
-		if(getOthers() == 1){
-			double fixaMira = relatividadeAngular(inimigo.getBearing() + (getHeading() - getRadarHeading()));
-			setTurnGunRight(fixaMira);
-		}
-		
-		if (getEnergy() > 70 && inimigo.getEnergy() < 40) {
-			jogaPraCimaDeles(inimigo);
-		} else if ((getEnergy() > 35 && getEnergy() <= 70) || (getEnergy() <= inimigo.getEnergy())) {
-			jogaNormal();
-		} else {
-			jogaNaRetranca();
-		}
-	}
+	/*
+	 * public void defineEstrategia(ScannedRobotEvent inimigo) { if (getEnergy()
+	 * > 55 && inimigo.getEnergy() < 30 && getOthers() == 1) {
+	 * jogaPraCimaDeles(inimigo); } else if ((getEnergy() > 35 && getEnergy() <=
+	 * 70) || (getEnergy() <= inimigo.getEnergy())) { jogaNormal(); } else {
+	 * jogaNaRetranca(); } }
+	 */
 
 	public void jogaPraCimaDeles(ScannedRobotEvent inimigo) {
 		distanciaInimigo = inimigo.getDistance();
 		anguloDoInimigo = inimigo.getBearing();
-		
+
 		turnRight(relatividadeAngular(anguloDoInimigo));
 		ahead(distanciaInimigo * 0.4);
-		tiroCerto(inimigo);
 	}
 
 	public void jogaNormal() {
 		setAhead(100);
-		setTurnRadarRight(90);
+		setTurnRadarRight(360);
 	}
 
 	public void jogaNaRetranca() {
@@ -146,12 +148,12 @@ public class Magneto extends AdvancedRobot {
 		setAhead(300);
 		turnRight(180);
 		setAhead(300);
-		
+
 	}
 
 	public boolean vaiBaterNaParede() {
-		return ((getX() < getBattleFieldWidth() - 50 || getX() > 50)
-				|| (getY() < getBattleFieldHeight() - 50 || getY() > 50));
+		return ((getX() > getBattleFieldWidth() - 50 || getX() < 50)
+				|| (getY() > getBattleFieldHeight() - 50 || getY() < 50));
 	}
 
 }
